b841f4476981d17bbe6a4c0e32e5d0e943ba6fe7
Tyler Mandry <tmandry@gmail.com>
2026-08-13 21:19:58 -0700

    (wip) add move_out example for Own

--- examples/simple/src/move_out.rs
+++ examples/simple/src/move_out.rs
@@ -12,6 +12,22 @@ fn move_out_desugared() {
     drop(bx.dev);
 }
 
+#[test]
+#[doc(hidden)]
+#[cfg(false)]
+fn move_out_own_desugared() {
+    use design::own::own;
+    let mut cfg = own!(BranchConfig {
+        main: "main".to_string(),
+        dev: "dev".to_string(),
+    });
+    let main = cfg.main;
+    print(main);
+    cfg.main = "trunk".to_string();
+
+    drop(cfg.dev);
+}
+
 /// Moving in and out of [`Box<T>`].
 ///
 /// ## Sugared Code
@@ -41,6 +57,72 @@ pub fn move_out() {
         main: "main".to_string(),
         dev: "dev".to_string(),
     });
+    let main = unsafe {
+        let hdl = LocalHandle::new(&raw mut bx);
+        let hdl = DerefPlace::deref_place(hdl);
+        let subplace = <field_of!(BranchConfig, main)>::default();
+        let hdl = ProjectPlace::project_place(hdl, subplace);
+        ReadPlace::read_place(hdl)
+    };
+    print(main);
+    unsafe {
+        let hdl = LocalHandle::new(&raw mut bx);
+        let hdl = DerefPlace::deref_place(hdl);
+        let subplace = <field_of!(BranchConfig, main)>::default();
+        let hdl = ProjectPlace::project_place(hdl, subplace);
+        WritePlace::write_place(hdl, "trunk".to_string())
+    };
+
+    drop(unsafe {
+        let hdl = LocalHandle::new(&raw mut bx);
+        let hdl = DerefPlace::deref_place(hdl);
+        let subplace = <field_of!(BranchConfig, dev)>::default();
+        let hdl = ProjectPlace::project_place(hdl, subplace);
+        ReadPlace::read_place(hdl)
+    });
+    unsafe {
+        let hdl = LocalHandle::new(&raw mut bx);
+        let hdl = DerefPlace::deref_place(hdl);
+        let subplace = <field_of!(BranchConfig, main)>::default();
+        let hdl = ProjectPlace::project_place(hdl, subplace);
+        DropPlace::drop_place(hdl)
+    };
+    unsafe { DropHusk::drop_husk(&raw mut bx) };
+    forget(bx);
+}
+
+/// Moving in and out of [`Own<T>`].
+///
+/// ## Sugared Code
+///
+/// ```
+/// let mut own = @Own BranchConfig {
+///     main: "main".to_string(),
+///     dev: "dev".to_string(),
+/// };
+/// let main = own.main;
+/// print(main);
+/// bx.main = "trunk".to_string();
+///
+/// drop(own.dev);
+/// ```
+///
+/// ## Desugared code
+///
+/// ```
+/// ==== DESUGARED ====
+/// ```
+///
+/// [`Box<T>`]: std::boxed::Box
+#[cfg(false)]
+#[cfg_attr(doc, design::utils::desugared)]
+pub fn move_out_own() {
+    let mut own = {
+        let v = BranchConfig {
+            main: "main".to_string(),
+            dev: "dev".to_string(),
+        };
+    };
     let main = unsafe {
         let hdl = LocalHandle::new(&raw mut bx);
         let hdl = DerefPlace::deref_place(hdl);
